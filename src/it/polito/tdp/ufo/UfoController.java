0d317a2962363efb72fd10369055790b7d7ccdbe
Federica <Federica@10.16.16.96>
2019-01-23 16:50:58 +0100

    Soluzione Punto A

--- src/it/polito/tdp/ufo/UfoController.java
+++ src/it/polito/tdp/ufo/UfoController.java
@@ -6,12 +6,17 @@ package it.polito.tdp.ufo;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.ufo.model.Model;
+import it.polito.tdp.ufo.model.SightingForYear;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 
 public class UfoController {
+	
+	Model model;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -20,21 +25,43 @@ public class UfoController {
     private URL location;
 
     @FXML // fx:id="boxAnno"
-    private ComboBox<?> boxAnno; // Value injected by FXMLLoader
+    private ComboBox<SightingForYear> boxAnno; // Value injected by FXMLLoader
 
     @FXML // fx:id="boxStato"
-    private ComboBox<?> boxStato; // Value injected by FXMLLoader
+    private ComboBox<String> boxStato; // Value injected by FXMLLoader
 
     @FXML // fx:id="txtResult"
     private TextArea txtResult; // Value injected by FXMLLoader
 
     @FXML
     void handleAnalizza(ActionEvent event) {
+    txtResult.clear();
+    	txtResult.appendText("Lista stati immediamente successivi a quello selezionato:\n");
+    	for(String s : this.model.getSuccessor(this.boxStato.getValue())) {
+    		if(!this.boxStato.getValue().equals(s))
+    		txtResult.appendText(s +"\n");
+    	}
+    	txtResult.appendText("Lista stati immediamente precedenti a quello selezionato:\n");
+    	for(String s : this.model.getPredecessor(this.boxStato.getValue())) {
+    		if(!this.boxStato.getValue().equals(s))
+    		txtResult.appendText(s +"\n");
+    	}
+    	txtResult.appendText("\nLista altri stati raggiungibili:\n");
+    	int conta = 0;
+    	for(String s : this.model.getStatiRaggiungibili(this.boxStato.getValue())) {
+    		if(!this.boxStato.getValue().equals(s)) {
+    		txtResult.appendText(s +"\n");
+    		conta++;}
+    	}
+    	txtResult.appendText("Numero stati raggiungibili "+ conta +"\n");
 
     }
 
     @FXML
     void handleAvvistamenti(ActionEvent event) {
+    	txtResult.clear();
+    	this.model.creaGrafo(this.boxAnno.getValue().getYear());
+    	this.boxStato.getItems().addAll(this.model.getState());
 
     }
 
@@ -50,4 +77,14 @@ public class UfoController {
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Ufo.fxml'.";
 
     }
+
+	public Model getModel() {
+		return model;
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
+		this.boxAnno.getItems().addAll(this.model.getYearAndCount());
+	}
+    
 }
--- src/it/polito/tdp/ufo/db/SightingsDAO.java
+++ src/it/polito/tdp/ufo/db/SightingsDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import it.polito.tdp.ufo.model.Sighting;
+import it.polito.tdp.ufo.model.SightingForYear;
 
 public class SightingsDAO {
 	
@@ -18,6 +19,72 @@ public class SightingsDAO {
 
 			PreparedStatement st = conn.prepareStatement(sql) ;
 			
+			List<Sighting> list = new ArrayList<>() ;
+			
+			ResultSet res = st.executeQuery() ;
+			
+			while(res.next()) {
+				list.add(new Sighting(res.getInt("id"),
+						res.getTimestamp("datetime").toLocalDateTime(),
+						res.getString("city"), 
+						res.getString("state"), 
+						res.getString("country"),
+						res.getString("shape"),
+						res.getInt("duration"),
+						res.getString("duration_hm"),
+						res.getString("comments"),
+						res.getDate("date_posted").toLocalDate(),
+						res.getDouble("latitude"), 
+						res.getDouble("longitude"))) ;
+			}
+			
+			conn.close();
+			return list ;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	public List<SightingForYear> getYearAndCount() {
+		String sql = "Select YEAR(datetime) as year, count(*) as count " + 
+				"from sighting " + 
+				"Where country = 'us' " + 
+				"Group by YEAR(datetime)" ;
+		try {
+			Connection conn = DBConnect.getConnection() ;
+
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			
+			List<SightingForYear> list = new ArrayList<>() ;
+			
+			ResultSet res = st.executeQuery() ;
+			
+			while(res.next()) {
+				list.add(new SightingForYear(res.getInt("year"), res.getInt("count"))) ;
+			}
+			
+			conn.close();
+			return list ;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	
+	public List<Sighting> getState(int year) {
+		String sql = "Select distinct * " + 
+				"From sighting " + 
+				"Where country = 'us' " + 
+				"and  YEAR(datetime) = ?" ;
+		try {
+			Connection conn = DBConnect.getConnection() ;
+
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			st.setInt(1, year);
 			List<Sighting> list = new ArrayList<>() ;
 			
 			ResultSet res = st.executeQuery() ;
